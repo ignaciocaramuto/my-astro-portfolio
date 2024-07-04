eb8923b9067170b6c82c16426fd16bab53bf2d2c
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-07-04 12:49:28 +0200

    fixed paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ignaciocaramuto.github.io",
-  base: "my-astro-repo",
+  base: "my-astro-portfolio",
 });
