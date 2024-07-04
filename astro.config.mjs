1c2e8f349d9feb61c146ae8878d0bd690b432960
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-07-04 12:08:39 +0200

    added site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://ignaciocaramuto.github.io",
+  base: "my-astro-repo",
+});
