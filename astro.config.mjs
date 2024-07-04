9a2ba9881c207315d4ab2a5b1b70b9165b1fc672
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-07-04 23:22:39 +0200

    removed base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ignaciocaramuto.github.io",
-  base: "my-astro-portfolio",
 });
